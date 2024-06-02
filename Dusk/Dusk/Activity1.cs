571d949ce6ae65d69d479b0115e158b42dbcdb95
BwdYeti <BwdYeti@gmail.com>
2024-06-02 11:41:36 -0400

    Initial commit of Dusk

---
+++ Dusk/Dusk/Activity1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace Android_MonoGame_Test
+{
+    [Activity(Label = "Dusk"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.Landscape //ScreenOrientation.SensorLandscape
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
+    public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException +=
+                new UnhandledExceptionEventHandler(Dusk.ExceptionLogger.Handler);
+
+            var g = new Dusk.Game1(new string[0]);
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+    }
+}
+
