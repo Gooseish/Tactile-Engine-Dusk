6290998cbba62b6e21f5fcf952fcd0846e98f16e
gooseish <cegooseish@gmail.com>
2025-05-20 20:45:44 -0400

    Added turn display but code that positions positioning the window pane is bad

--- Tactile/Source Code/Windows/Turnwheel/Window_Turnwheel.cs
+++ Tactile/Source Code/Windows/Turnwheel/Window_Turnwheel.cs
@@ -1,20 +1,24 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Tactile.Graphics.Text;
-using TactileLibrary;
+using Tactile.Graphics.Windows;
+
 
 namespace Tactile.Windows
 {
     class Window_Turnwheel : Stereoscopic_Graphic_Object
     {
+        protected WindowPanel Window_Img;
         protected TextSprite Turn_Number;
         public int turn_number { set { Turn_Number.text = "Turn: " + value.ToString(); } }
+
         public Window_Turnwheel()
         {
             Turn_Number = new TextSprite();
             Turn_Number.SetFont(Config.UI_FONT, Global.Content, "White");
+            Window_Img = new WindowPanel(Global.Content.Load<Texture2D>(System_Color_Window.FILENAME));
+            Window_Img.width = 55;
+            Window_Img.height = 32;
         }
 
         public void draw(SpriteBatch sprite_batch)
@@ -28,6 +32,7 @@ namespace Tactile.Windows
         public virtual void draw(SpriteBatch sprite_batch, Vector2 draw_offset, RasterizerState state)
         {
             sprite_batch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, state);
+            Window_Img.draw(sprite_batch, draw_offset - (loc + draw_vector() + new Vector2(8, -7) - offset));
             Turn_Number.draw(sprite_batch, draw_offset - (loc + draw_vector() + new Vector2(16, 0) - offset));
             sprite_batch.End();
         }
