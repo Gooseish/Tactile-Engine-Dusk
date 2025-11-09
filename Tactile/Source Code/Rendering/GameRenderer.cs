705123e3ff86a705843a35de8f985254d72edd18
gooseish <cegooseish@gmail.com>
2025-11-09 15:11:07 -0500

    band-aid for threading issues?

--- Tactile/Source Code/Rendering/GameRenderer.cs
+++ Tactile/Source Code/Rendering/GameRenderer.cs
@@ -49,7 +49,8 @@ namespace Tactile.Rendering
         private Vector2 TouchCursorLoc;
         private Texture2D MouseCursorTexture;
 
-        private Thread LightmapThread; 
+        private Thread LightmapThread;
+        private bool Drawing;
 
         public static int zoom
         {
@@ -399,6 +400,8 @@ namespace Tactile.Rendering
         #region Draw
         public void Draw()
         {
+            wait_for_lightmap();
+            Drawing = true;
             // Reset data
             Global.palette_pool.update();
             if (ShaderRenderTargets[0] == null)
@@ -409,8 +412,6 @@ namespace Tactile.Rendering
             camera.offset = new Vector2(ShaderRenderTargets[0].Width / 2, ShaderRenderTargets[0].Height / 2);
             Vector2 ratio = new Vector2(ScreenSizeRatio);
             camera.zoom = ratio;
-
-            wait_for_lightmap();
 
             // Always draw the screen normally to FinalRender, for screenshotting/suspend images
             DrawScene(spriteBatch, Stereoscopic_Mode.Center);
@@ -458,16 +459,26 @@ namespace Tactile.Rendering
             }
 
             GraphicsDevice.SetRenderTarget(null);
+            Drawing = false;
         }
 
         private void draw_lightmap()
         {
+            wait_for_drawing();
             while (true)
             {
                 if (Global.game_map != null && Global.game_map.lightmap_needs_redrawing)
                     Global.game_map.get_scene_map().draw_lightmap(spriteBatch, GraphicsDevice);
                 else
                     System.Threading.Thread.Sleep(
+                        TimeSpan.FromTicks((int)(TimeSpan.TicksPerSecond * (1.0f / ((float)Config.FRAME_RATE * 10)))));
+            }
+        }
+        private void wait_for_drawing()
+        {
+            while (Drawing)
+            {
+                System.Threading.Thread.Sleep(
                         TimeSpan.FromTicks((int)(TimeSpan.TicksPerSecond * (1.0f / ((float)Config.FRAME_RATE * 10)))));
             }
         }
